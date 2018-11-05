f12e0fc750c291e6dbddaa53873abb412ddae8fc
JordiPeris <jordiperis@gmail.com>
2018-11-05 19:35:37 +0100

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public void TestButton() {
+        print(References.Instance.testGO.name);
+    }
+}
